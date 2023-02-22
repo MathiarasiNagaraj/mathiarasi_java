e84093da46b905d236e208132f6e8c862a55c218
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-02-22 22:05:33 +0530

    updated naming conventions

--- Exercise 2/EX1_PassByReference.java
+++ Exercise 2/EX1_PassByReference.java
@@ -1,39 +1,39 @@
 package Exercise_2;
 
 public class EX1_PassByReference {
-	int val=23;
+	int value=23;
 public static void main(String[] args) {
 	//Primitive types
-	int a=10;
-	System.out.println("before changing A value in function :"+a);
-	change_A_Value(a);
-	System.out.println("After changing A value in function :"+a);
+	int primitive_variable=10;
+	System.out.println("before changing A value in function :"+primitive_variable);
+	changePrimitiveType(a);
+	System.out.println("After changing A value in function :"+primitive_variable);
 	System.out.println("Thus primitive values are passing as value..");
 	//array
-	int arr[]=new int[] {1,2,3,4};
-	System.out.println("Printing array values before changing : "+'\n'+arr[0]+'\t'+arr[1]+'\t'+arr[2]+'\t'+arr[3]);
-	change_Array(arr);
-	System.out.println("Printing array values after changing : "+'\n'+arr[0]+'\t'+arr[1]+'\t'+arr[2]+'\t'+arr[3]);
+	int array[]=new int[] {1,2,3,4};
+	System.out.println("Printing array values before changing : "+'\n'+array[0]+'\t'+array[1]+'\t'+array[2]+'\t'+array[3]);
+	changeReferenceTypeArray(arr);
+	System.out.println("Printing array values after changing : "+'\n'+array[0]+'\t'+array[1]+'\t'+array[2]+'\t'+array[3]);
 	EX1_PassByReference obj=new EX1_PassByReference();
 	System.out.println("Printing class variable before changing \n"+obj.val);
 	System.out.println("Passing object of the class to function ..\n");
-	change_Object(obj);
+	changeReferenceTypeObject(obj);
 	System.out.println("Printing class variable after changing \n"+obj.val);
 	
 }
-public static void change_A_Value(int a)
+public static void changePrimitiveType(int variable_primitive)
 {
-	a=23;
-	System.out.println("In change A function :"+a);
+	variable_primitive=23;
+	System.out.println("In change A function :"+variable_primitive);
 }
-public static void change_Array(int arr[])
+public static void changeReferenceTypeArray(int array[])
 {
-	arr[0]=23;
-	arr[1]=45;
-	arr[2]=56;
-	System.out.println("Changing array value in function : "+'\n'+arr[0]+'\t'+arr[1]+'\t'+arr[2]+'\t'+arr[3]);
+	array[0]=23;
+	array[1]=45;
+	array[2]=56;
+	System.out.println("Changing array value in function : "+'\n'+array[0]+'\t'+array[1]+'\t'+array[2]+'\t'+array[3]);
 }
-public static void change_Object(EX1_PassByReference object)
+public static void changeReferenceTypeObject(EX1_PassByReference object)
 {
 	object.val=789;
 	System.out.println("Printing class variable in chaning  function \n"+object.val);
