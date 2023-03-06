8beeb19a634d17247eb40803fe2873c7acee24db
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-03-06 11:11:14 +0530

    Update Video.java

--- Bridge pattern/Video.java
+++ Bridge pattern/Video.java
@@ -1,10 +1,21 @@
-package Pattern;
+package BridgePattern;
 
+/*
+ * Video class Processor as property
+ * and play method
+ * **/
 public abstract class Video {
+	/*
+	 * Reference of VideoProcessor is acting as bridge between video and videoProcessor
+	 * **/
  VideoProcessor processor;
+ /*
+  * constructor to set the processor of the video **/
  public Video(VideoProcessor processor)
  {
 	 this.processor=processor;
  }
+ /*
+  * play to play the file**/
  public abstract void play(String videoFile);
 }
