9257dbe95b1bdf76335886d2ec69ccf4293f4d38
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-03-06 11:10:54 +0530

    Update Instagram.java

--- Bridge pattern/Instagram.java
+++ Bridge pattern/Instagram.java
@@ -1,13 +1,15 @@
-package Pattern;
-
+package BridgePattern;
+/*
+ * Instagram is class that extends video **/
 public class Instagram extends Video{
 	 public Instagram(VideoProcessor processor) {
 	        super(processor);
 	    }
 
+	/**Processed as per given processor
+     Now play*/
 	    @Override
 	    public void play(String videoFile) {
-	        processor.process(videoFile); //Processed as per given processor
-	        //Now play
+	        processor.process(videoFile); 
 	    }
 }
