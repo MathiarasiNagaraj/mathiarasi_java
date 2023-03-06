ec75f7d1bba1da004364d92f8e8a75f7d170ded7
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-03-06 11:12:21 +0530

    Update Youtube.java

--- Bridge pattern/Youtube.java
+++ Bridge pattern/Youtube.java
@@ -1,13 +1,15 @@
-package Pattern;
+package BridgePattern;
 
+/*
+ * Youtube is one kind of video**/
 public class Youtube extends Video{
 	 public Youtube(VideoProcessor processor) {
 	        super(processor);
 	    }
-
+	/**Processed as per given processor
+     /Now play*/
 	    @Override
 	    public void play(String videoFile) {
-	        processor.process(videoFile); //Processed as per given processor
-	        //Now play
+	        processor.process(videoFile); 
 	    }
 }
