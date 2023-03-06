fd39b60aa6ec18afbb187068ca1934e2c8e52e91
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-03-06 11:12:55 +0530

    Update MementoPattern.java

--- Memento pattern/MementoPattern.java
+++ Memento pattern/MementoPattern.java
@@ -1,19 +1,33 @@
-package Pattern;
+package MementoPattern;
 
 import java.io.File;
 import java.util.Stack;
 
 public class MementoPattern {
 public static void main(String[] args) {
+	/**
+	 * creating a newAccount */
 	SocialMedia newAccount=new SocialMedia();
 	
 
 	File file = new File("path");
+	/*
+	 * setting bio and profile for a account using setters
+	 * **/
+	
 	newAccount.setFile(file);
     newAccount.setBio("hai this is new account");
+    /*
+     * showing the details**/
     System.out.println(newAccount.showBio());
+    /*
+     * changing the details of bio **/
     newAccount.setBio("changed state");
+    /*
+     * showing the details after changing**/
     System.out.println(newAccount.showBio());
+    /*
+     * using undobio to change method to revert the change and get previous state of the object**/
     newAccount.undobio();
     
     System.out.println(newAccount.showBio());
