ed941db38f4c63a75dc8482acdecad514865efaa
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-03-06 11:07:35 +0530

    Update AmazonPrime.java

--- Bridge pattern/AmazonPrime.java
+++ Bridge pattern/AmazonPrime.java
@@ -1,13 +1,17 @@
-package Pattern;
+package BridgePattern;
 
 public class AmazonPrime extends Video{
+	/*
+	 * Amazon prime is class that extends video **/
 	 public AmazonPrime(VideoProcessor processor) {
 	        super(processor);
 	    }
-
+	 /**Processed as per given processor
+	  * 
+     Now play*/
 	    @Override
 	    public void play(String videoFile) {
-	        processor.process(videoFile); //Processed as per given processor
-	        //Now play
+	        processor.process(videoFile); 
+	       
 	    }
 }
