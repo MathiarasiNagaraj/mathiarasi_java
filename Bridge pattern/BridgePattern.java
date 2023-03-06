38439263c72963f1d8a47e8873d436c3da045c1a
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-03-06 11:10:13 +0530

    Update BridgePattern.java

--- Bridge pattern/BridgePattern.java
+++ Bridge pattern/BridgePattern.java
@@ -1,12 +1,14 @@
-package Pattern;
+package BridgePattern;
 
 public class BridgePattern {
-public static void main(String[] args) {
-	Video youtube=new Youtube(new HDProcessor());
-	youtube.play("Beliver.mp4");
-	Video instagram=new Instagram(new UHD4KProcessor());
-	instagram.play("Beliver.mp4");
-	Video prime=new AmazonPrime(new UHD18KProcessor());
-	prime.play("Beliver.mp4");
-}
+	public static void main(String[] args) {
+		/*
+		 * creating different implementation of Video objects **/
+		Video youtube = new Youtube(new HDProcessor());
+		youtube.play("Beliver.mp4");
+		Video instagram = new Instagram(new UHD4KProcessor());
+		instagram.play("Beliver.mp4");
+		Video prime = new AmazonPrime(new UHD18KProcessor());
+		prime.play("Beliver.mp4");
+	}
 }
