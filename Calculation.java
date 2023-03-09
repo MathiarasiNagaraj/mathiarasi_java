b5cc30376d91c5ba600e35b6e8d4e884c90aa8c6
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-03-09 23:19:27 +0530

    updated with java doc

--- Calculation.java
+++ Calculation.java
@@ -7,32 +7,46 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Calculation {
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		float speed, distance;
+		double speed, distance,workingHour;
 		String date, time;
 	
 		LocalDateTime start=null;
 		LorryDriver driver=new LorryDriver();
+		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter speed of driving:");
-		speed = scan.nextFloat();
-		System.out.println("Enter distance to cover:");
-		distance = scan.nextFloat();
+		//getting details from the user
+		System.out.println("Enter speed of driving in km/hr:");
+		
+		speed = scan.nextDouble();
+		System.out.println("Enter distance to cover in km:");
+		distance = scan.nextDouble();
+		System.out.println("Enter the working hour in a day excluding break hours:");
+		workingHour = scan.nextDouble();
 		System.out.println("Enter the travelling date in DD/MM/YYYY format :");
 		date = scan.next();
 		
 		System.out.println("Enter the travelling time in HH:MM format");
 		time = scan.next();
 		
+		//setting driver with speed and working hours
+		
 		driver.setDrivingSpeed(speed);
-		driver.setWorkingHours(8);
+		driver.setWorkingHours(workingHour);
 		System.out.println(date+" "+time);
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
 		try {
 			start = LocalDateTime.parse(date+" "+time, format);
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			System.out.println("Enter date correct format !!!");
 		}
+		//passing distance,start as datetime object,driver object
 		TravelTime timetaken=new TravelTime(distance,start,driver);
 		timetaken.calculateTime();
 		scan.close();
